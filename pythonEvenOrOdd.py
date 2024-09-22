e04ba84c5948ddb7c3eee96f0ed099227a69dcd1
preston <polston13@outlook.com>
2024-09-22 15:37:43 -0400

    finished project

---
+++ pythonEvenOrOdd.py
@@ -0,0 +1,7 @@
+# create a program that takes users input number and returns even or odd
+
+inputNumber = float(input("Please enter a number: "))
+if (inputNumber % 2 == 0):
+    print("your number is even")
+if(inputNumber % 2 == 1):
+    print("your number is odd")
